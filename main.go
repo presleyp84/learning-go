5a22124b540d001d0ce798dc1b19deb67aff2a57
Patrick Presley <57566465+presleyp84@users.noreply.github.com>
2022-03-08 19:47:47 +0530

    added ticket count logic

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 
 // main function
 func main() {
-	//declatation of variables and conctants
+	//declatation of variables and constants
 	appName := "Concert Booking Application" // using : instead of var. cannot be used to declare constants
+	eventname := "Mettalica"
 	const concertTickets int = 100
 	var remainingTickets uint = 100 //unsigned int i.e cannot be negative
 
@@ -17,22 +18,26 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	//using printf statement to print in a specific format. print or println can also be used
 	fmt.Printf("Hello and Welcome to %v\n", appName)
 	fmt.Printf("Please book your tickets! %v tickets available\n", remainingTickets)
 	fmt.Printf("The time is %v\n", time.Now())
 
-	// Ask user to input data
+	// Assigning values based on user input data
 	fmt.Printf("Enter your First Name:\n")
-	fmt.Scan(&firstName)
+	fmt.Scan(&firstName) //pointers
 	fmt.Printf("Enter your Last Name:\n")
 	fmt.Scan(&lastName)
 	fmt.Printf("Enter your E-mail address:\n")
 	fmt.Scan(&email)
-	fmt.Printf("\nEnter number of tickets to book:\n")
+	fmt.Printf("Enter number of tickets to book:\n")
 	fmt.Scan(&userTickets)
 
+	//ticket count logic
+	remainingTickets = remainingTickets - userTickets
+
 	fmt.Printf("Thank you  %v %v for booking %v ticket(s). You will receive a confirmation E-mail at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v Remaining tickets for %v concert\n", remainingTickets, eventname)
 }
