763d56582eee55ad9a279fc58d3ab78ea097df0e
Patrick Presley <57566465+presleyp84@users.noreply.github.com>
2022-03-08 19:25:38 +0530

    implemented user input

--- main.go
+++ main.go
@@ -13,16 +13,26 @@ func main() {
 	const concertTickets int = 100
 	var remainingTickets uint = 100 //unsigned int i.e cannot be negative
 
+	//user input vars
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+
 	//using printf statement to print in a specific format. print or println can also be used
 	fmt.Printf("Hello and Welcome to %v\n", appName)
 	fmt.Printf("Please book your tickets! %v tickets available\n", remainingTickets)
 	fmt.Printf("The time is %v\n", time.Now())
 
-	var userName string
-	var userTickets int
+	// Ask user to input data
+	fmt.Printf("Enter your First Name:\n")
+	fmt.Scan(&firstName)
+	fmt.Printf("Enter your Last Name:\n")
+	fmt.Scan(&lastName)
+	fmt.Printf("Enter your E-mail address:\n")
+	fmt.Scan(&email)
+	fmt.Printf("\nEnter number of tickets to book:\n")
+	fmt.Scan(&userTickets)
 
-	// user input
-	userName = "Pat"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+	fmt.Printf("Thank you  %v %v for booking %v ticket(s). You will receive a confirmation E-mail at %v\n", firstName, lastName, userTickets, email)
 }
